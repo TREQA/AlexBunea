c6d22e97e9a8c3267caa22f182a7d4fe3c311839
Alexandru Bunea <alexandru.bunea@tremend.com>
2017-12-19 18:43:02 +0200

    mesaj

---
+++ src/HW03_02_Sintax2.java
@@ -0,0 +1,31 @@
+/*
+Se dă codul următor:
+        int a=2,b=3;
+        System.out.print("First operand: ");
+        System.out.println(a);
+        System.out.print("Second operand: ");
+        System.out.println(b);
+        System.out.print("Sum: ");
+        System.out.println(a+b);
+
+Codul dă următoarea ieşire:
+        First operand: 2
+        Second operand: 3
+        Sum: 5
+Trebuie să ne asigurăm că la ieşire codul afişează doar rezultatul
+(numărul 5) şi că toate liniile de cod rămân disponibile pentru o
+utilizare viitoare.
+*/
+public class HW03_02_Sintax2 {
+    public static void main(String[] args){
+int a=2, b=3;
+/*
+System.out.print("First operand:");
+System.out.println(a);
+System.out.print("Second operand:");
+System.out.println(b);
+System.out.print("Sum= ");
+*/
+System.out.println(a+b);
+    }
+}
