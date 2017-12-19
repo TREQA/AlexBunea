66f61df40014da6b3624a98481edb05f9bfb43d0
Alexandru Bunea <alexandru.bunea@tremend.com>
2017-12-19 18:29:04 +0200

    mesaj

---
+++ src/HW03_01_Sintax.java
@@ -0,0 +1,30 @@
+/*
+Se dă următorul cod care adună două numere:
+
+int operand1=2;
+int operand2=3;
+int sum=operand1+operand2;
+System.out.println(sum);
+
+Trebuie să facem o descriere a codului pentru documentație.
+ */
+public class HW03_01_Sintax {
+    public static void main(String[] args) {
+        /*
+         * primul sumator
+         */
+        int operand1 = 1;
+        /*
+         * al doilea sumator
+         */
+        int operand2 = 2;
+        /*
+         * calculul adunarii
+         */
+        int sum = operand1 + operand2;
+        /*
+         * scrierea rezultatului
+         */
+        System.out.println(sum);
+    }
+}
