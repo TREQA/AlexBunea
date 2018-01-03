331a08d2ef6bc7f4029eb1d35af68b243461719a
Alexandru Bunea <alexandru.bunea@tremend.com>
2018-01-03 17:27:10 +0200

    mesaj

---
+++ src/HW_02_03_ex01_Operatori.java
@@ -0,0 +1,15 @@
+/*
+Este necesar să creăm un program care adună două numere reprezentate prin două variabile.
+Primul număr este variabila x, cu valoarea 1,
+iar celălalt este variabila y, cu valoarea 2.
+Trebuie să atribuim variabilei z rezultatul adunării acestor două variabile.
+ */
+public class HW_02_03_ex01_Operatori {
+    public static void main (String[] args)
+    {
+        int x=1;
+        int y=2;
+        int z=x+y;
+        System.out.println(z);
+    }
+}
