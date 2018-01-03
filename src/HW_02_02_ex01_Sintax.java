15a004522909819a82046157b7c564e76417cd23
Alexandru Bunea <alexandru.bunea@tremend.com>
2018-01-03 15:23:29 +0200

    mesaj

---
+++ src/HW_02_02_ex01_Sintax.java
@@ -0,0 +1,17 @@
+/*
+Exercițiul 1
+Numele variabilelor trebuie schimbate astfel încât să fie corecte
+sintactic şi în acord cu convenţiile:
+
+String 1stUserName;
+int bdt; //date of birthday
+float o1; //first operand
+ */
+public class HW_02_02_ex01_Sintax {
+    public static void main (String[] args)
+    {
+        String firstUserName;
+        int birthDate;
+        float operand1;
+    }
+}
