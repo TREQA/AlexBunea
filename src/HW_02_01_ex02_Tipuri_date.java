83d3edf861ecdbab4f3b30ce7d3a7241e8eafd2f
Alexandru Bunea <alexandru.bunea@tremend.com>
2018-01-03 11:52:10 +0200

    mesaj

--- src/HW_02_01_ex02_Tipuri_date.java
+++ src/HW_02_01_ex02_Tipuri_date.java
@@ -4,7 +4,7 @@ O variabilă de tip double cu valoarea 15,757 și o variabilă întreagă de tip
 Trebuie să convertiți variabila cu virgulă mobilă într-o valoare întreagă.
  */
 
-public class HW_02_01_ex01_Tipuri_date {
+public class HW_02_01_ex02_Tipuri_date {
     public static void main (String[] args)
     {
         double x = 15.757;
