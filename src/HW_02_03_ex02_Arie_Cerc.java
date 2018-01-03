0bbb0386bf82d804a17f009f66321d7ed3998cd8
Alexandru Bunea <alexandru.bunea@tremend.com>
2018-01-03 17:42:18 +0200

    mesaj

---
+++ src/HW_02_03_ex02_Arie_Cerc.java
@@ -0,0 +1,12 @@
+/*
+Trebuie să creăm un program care calculează aria unui cerc.
+ */
+public class HW_02_03_ex02_Arie_Cerc {
+    static final double PI=3.14;
+    public static void main (String[] args)
+    {
+        double R=5;
+        double aria= PI * (R*R);
+        System.out.println("Aria cercului cu raza de 5cm este: " + aria + "cm2");
+    }
+}
