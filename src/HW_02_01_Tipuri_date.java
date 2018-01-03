d72d821456d08c5f76b991227decd71a7b7b2f3a
Alexandru Bunea <alexandru.bunea@tremend.com>
2018-01-03 11:34:58 +0200

    mesaj

---
+++ src/HW_02_01_Tipuri_date.java
@@ -0,0 +1,14 @@
+/*
+Trebuie să declarăm două variabile.
+O variabilă de tip double cu valoarea 15,757 și o variabilă întreagă de tip int.
+Trebuie să convertiți variabila cu virgulă mobilă într-o valoare întreagă.
+ */
+
+public class HW_02_01_Tipuri_date {
+    public static void main (String[] args)
+    {
+        double x = 15.757;
+        int y = (int) x;
+        System.out.println(y);
+    }
+}
